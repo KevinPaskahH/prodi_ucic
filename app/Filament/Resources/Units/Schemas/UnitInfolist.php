7748fb3cf95c147b33cdd7b31e0709e9073658c7
Avini Nanda Hanifah <avininandahanifah@MacBook-Air-Avini.local>
2025-11-19 11:49:47 +0700

    Menambahkan Unit resource

---
+++ app/Filament/Resources/Units/Schemas/UnitInfolist.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filament\Resources\Units\Schemas;
+
+use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Schema;
+
+class UnitInfolist
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextEntry::make('level_id')
+                    ->numeric(),
+                TextEntry::make('name'),
+                TextEntry::make('slug'),
+                TextEntry::make('created_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+                TextEntry::make('updated_at')
+                    ->dateTime()
+                    ->placeholder('-'),
+            ]);
+    }
+}
